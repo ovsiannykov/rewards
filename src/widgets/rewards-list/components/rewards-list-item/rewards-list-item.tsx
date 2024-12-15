3748ce99844ff925745a74130e1099158cdfea71
ovsiannykov.dev@gmail.com <ovsiannykov.dev@gmail.com>
2024-12-15 19:21:35 +0100

    Added basic logic of saving rewards

--- src/widgets/rewards-list/components/rewards-list-item/rewards-list-item.tsx
+++ src/widgets/rewards-list/components/rewards-list-item/rewards-list-item.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 
+import { useSelector } from 'react-redux'
+import { useRewards } from '../../../../entities/rewards'
 import HeartFillSvg from '../../../../shared/assets/heart-fill.svg'
 import HeartSvg from '../../../../shared/assets/heart.svg'
 import { HIT_SLOP_15 } from '../../../../shared/constants'
-import { RewardType } from '../../../../shared/types'
+import { RewardType, RootState } from '../../../../shared/types'
 import styles from './rewards-list-item.styles'
 
 interface RewardsListItemProps {
@@ -12,15 +14,28 @@ interface RewardsListItemProps {
 }
 
 export function RewardsListItem({ item }: RewardsListItemProps) {
-	// temporary:
-	const isCollected = false
+	const { collectedRewards } = useSelector((state: RootState) => state.rewards)
+	const { handleCollectReward, handleRemoveCollectedReward } = useRewards()
+
+	const isCollected = useMemo(
+		() => collectedRewards.some((reward) => reward.id === item.id),
+		[collectedRewards, item.id]
+	)
+
+	const handleToggleCollect = () => {
+		if (isCollected) {
+			handleRemoveCollectedReward(item.id)
+		} else {
+			handleCollectReward(item)
+		}
+	}
 
 	return (
 		<TouchableOpacity style={styles.box}>
 			<Text style={styles.title} numberOfLines={2}>
 				{item.name}
 			</Text>
-			<TouchableOpacity hitSlop={HIT_SLOP_15}>
+			<TouchableOpacity hitSlop={HIT_SLOP_15} onPress={handleToggleCollect}>
 				{isCollected ? (
 					<HeartFillSvg width={24} height={24} />
 				) : (
