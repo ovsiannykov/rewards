ff087087b156ac63ced397e85b40b17446f39b08
ovsiannykov.dev@gmail.com <ovsiannykov.dev@gmail.com>
2024-12-16 10:25:37 +0100

    Added tabs icons

--- src/navigation/tab-navigator/tab-navigator.tsx
+++ src/navigation/tab-navigator/tab-navigator.tsx
@@ -2,14 +2,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { FavoritesScreen } from '../../screens/favorites-screen'
 import { HomeScreen } from '../../screens/home'
+import HeartFillSvg from '../../shared/assets/heart-fill.svg'
+import HeartSvg from '../../shared/assets/heart.svg'
+import HomeFillSvg from '../../shared/assets/home-fill.svg'
+import HomeSvg from '../../shared/assets/home.svg'
 
 const Tab = createBottomTabNavigator()
 
-// #Todo: add tabs icons
+const getTabBarIcon = (routeName: string, focused: boolean) => {
+	switch (routeName) {
+		case 'HOME':
+			return focused ? <HomeFillSvg /> : <HomeSvg />
+		case 'FAVORITES':
+			return focused ? <HeartFillSvg /> : <HeartSvg />
+		default:
+			return null
+	}
+}
+
+const screenOptions = ({ route }: { route: { name: string } }) => ({
+	tabBarIcon: ({ focused }: { focused: boolean }) =>
+		getTabBarIcon(route.name, focused),
+	tabBarActiveTintColor: 'black',
+	tabBarInactiveTintColor: 'black',
+})
 
 export function TabNavigator() {
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator screenOptions={screenOptions}>
 			<Tab.Screen name="HOME" component={HomeScreen} />
 			<Tab.Screen name="FAVORITES" component={FavoritesScreen} />
 		</Tab.Navigator>
